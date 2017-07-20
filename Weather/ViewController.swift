802b904b86b00edcdf2b6079a404b997a00df01d
mayflight <18868824465@163.com>
2017-07-21 00:25:42 +0800

    新建工程

---
+++ Weather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by yadong.ye on 2017/7/21.
+//  Copyright © 2017年 yadong.ye. All rights reserved.
+//
+
+//http://www.sojson.com/open/api/weather/json.shtml?city=杭州
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
